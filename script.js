84a6ed6a74a84326b276ad0c883d17669f95dc03
Shivanshu Mishra <163833644+shiva6307@users.noreply.github.com>
2024-08-10 11:44:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentIndex = 0;
+
+function showImage(index) {
+    const gallery = document.querySelector('.gallery');
+    const images = document.querySelectorAll('.gallery img');
+    if (index >= images.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = images.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    gallery.style.transform = `translateX(${offset}%)`;
+}
+
+function nextImage() {
+    showImage(currentIndex + 1);
+}
+
+function prevImage() {
+    showImage(currentIndex - 1);
+}
